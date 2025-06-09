e943d52e4a36a9752cca0509efb32485d9083660
samadhan <samadhan.mhaske@frnd.app>
2025-06-09 19:26:50 +0530

    hellow

---
+++ channel_with_rabbitmq/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for channel_with_rabbitmq project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channel_with_rabbitmq.settings')
+
+application = get_asgi_application()
